737e3dd4819c2f1e15fe7663e11c4d0597f6b57e
niish03 <nishitpatel.nnp@gmail.com>
2021-06-08 00:51:05 +0530

    Dark/light mode added

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 var typed = new Typed('.typing', {
-    strings:['Web developer', 'Flutter developer','UI/UX designer' , 'Graphic desiner','Freelancer' ,'Photographer' ],
-backSpeed: 50,
-typeSpeed: 50,
-loop:true
-  });
+  strings: ['Web developer', 'Flutter developer', 'UI/UX designer', 'Graphic desiner', 'Freelancer', 'Photographer'],
+  backSpeed: 50,
+  typeSpeed: 50,
+  loop: true
+});
+var x = 'dark';
+function changetheme() {
+  document.querySelectorAll('.cngthm').forEach((result) => {
+      result.classList.toggle('dark-mode');
+      
+      if (x == 'dark') {
+        document.body.style.setProperty('--bgclr', '#fff');
+        x= 'light';
+      } else if (x == 'light') {
+        document.body.style.setProperty('--bgclr', '#161616');
+        x= 'dark';
+      }
+
+  })
+
+  document.querySelectorAll('.skip-cng').forEach((result) => {
+      result.classList.toggle('no-cng');
+  })
+
+
+
+};
