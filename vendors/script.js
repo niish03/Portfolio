0c7defe15bf7b9cac255cc3a6c1a1f25e3df5a9c
niish03 <nishitpatel.nnp@gmail.com>
2021-06-10 03:34:46 +0530

    a

--- vendors/script.js
+++ vendors/script.js
@@ -1,6 +1,6 @@
 var prevScrollpos = window.pageYOffset;
 
-window.onscroll = function() {
+window.onscroll = function () {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
@@ -11,10 +11,10 @@ window.onscroll = function() {
   prevScrollpos = currentScrollPos;
 }
 
-document.getElementById('navbar-mobile').addEventListener("click" ,()=>{
-  if(window.innerWidth < 990)
-  document.getElementById('navbar-toggler').click();
-  } )
+document.getElementById('navbar-mobile').addEventListener("click", () => {
+  if (window.innerWidth < 990)
+    document.getElementById('navbar-toggler').click();
+})
 
 var typed = new Typed('.typing', {
   strings: ['Web developer', 'Flutter developer', 'UI/UX designer', 'Graphic desiner', 'Freelancer', 'Photographer'],
@@ -23,32 +23,35 @@ var typed = new Typed('.typing', {
   loop: true
 });
 var x = 'dark';
+
 function changetheme() {
-  
+
   document.querySelectorAll('.cngthm').forEach((result) => {
-      result.classList.toggle('dark-mode');
-      
-      
+    result.classList.toggle('dark-mode');
+
+
 
   })
   if (x == 'dark') {
     document.body.style.setProperty('--bgclr', '#fff');
     document.body.style.setProperty('--navbarbg', '#ffffffe7');
-    $('i[name="dark-lighticon"]').removeClass('uil-moon').addClass('uil-brightness').css('color','#161616');
+    document.body.style.setProperty('--contentbg', '#6363ff1a');
+    $('i[name="dark-lighticon"]').removeClass('uil-brightness').addClass('uil-moon').css('color', '#161616');
     $('i[name="appsicon"]').addClass('light');
-    
-    
-    x= 'light';
-  } else if(x == 'light') {
+
+
+    x = 'light';
+  } else if (x == 'light') {
     document.body.style.setProperty('--bgclr', '#161616');
     document.body.style.setProperty('--navbarbg', '#2b2b2be5');
-    $('i[name="dark-lighticon"]').removeClass('uil-brightness').addClass('uil-moon').css('color','#fff');
+    document.body.style.setProperty('--contentbg', '#2b2b2be5');
+    $('i[name="dark-lighticon"]').removeClass('uil-moon').addClass('uil-brightness').css('color', '#fff');
     $('i[name="appsicon"]').removeClass('light');
-    x= 'dark';
+    x = 'dark';
   }
 
   document.querySelectorAll('.skip-cng').forEach((result) => {
-      result.classList.toggle('no-cng');
+    result.classList.toggle('no-cng');
   })
 };
 
@@ -59,34 +62,44 @@ function changetheme() {
 
 
 //menu navbar
-document.getElementById("navbar-toggler").addEventListener('click' , ()=> {  
+document.getElementById("navbar-toggler").addEventListener('click', () => {
   $('i[name="menu-icons"]').removeClass('Visually-hidden');
 
 });
 
+//skills
 
-/*top back*/
-const onscroll = (el, listener) => {
-  el.addEventListener('scroll', listener)
-}
+$('#skill1dd').click(()=>{
+  
+  if (!$('#skill1dd').hasClass("collapsed")) 
+    $('#skill1').addClass("uil-angle-up").removeClass('uil-angle-down')
+  if($('#skill1dd').hasClass("collapsed"))
+    $('#skill1').addClass("uil-angle-down").removeClass('uil-angle-up')
+  
+})
 
-const select = (el, all = false) => {
-  el = el.trim()
-  if (all) {
-    return [...document.querySelectorAll(el)]
-  } else {
-    return document.querySelector(el)
-  }
-}
-let backtotop = select('.back-to-top')
-  if (backtotop) {
-    const toggleBacktotop = () => {
-      if (window.scrollY > 100) {
-        backtotop.classList.add('active')
+    /*top back*/
+    const onscroll = (el, listener) => {
+      el.addEventListener('scroll', listener)
+    }
+
+    const select = (el, all = false) => {
+      el = el.trim()
+      if (all) {
+        return [...document.querySelectorAll(el)]
       } else {
-        backtotop.classList.remove('active')
+        return document.querySelector(el)
       }
     }
-    window.addEventListener('load', toggleBacktotop)
-    onscroll(document, toggleBacktotop)
-  }
+    let backtotop = select('.back-to-top')
+    if (backtotop) {
+      const toggleBacktotop = () => {
+        if (window.scrollY > 100) {
+          backtotop.classList.add('active')
+        } else {
+          backtotop.classList.remove('active')
+        }
+      }
+      window.addEventListener('load', toggleBacktotop)
+      onscroll(document, toggleBacktotop)
+    }
